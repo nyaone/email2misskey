b564f1fe789c9fa76b050a18775b85f8c1bad36a
Nya Candy <dev@candinya.com>
2023-02-18 09:42:38 +0800

    chore: tweak message template

--- consts/message_template.go
+++ consts/message_template.go
@@ -1,6 +1,6 @@
 package consts
 
 const (
-	SummaryTemplate = "新邮件来啦！ #email 标题：%s"
-	MessageTemplate = "发件人是 %s\n\n您可以[点击这里](%s)预览完整内容，或者下载附件查看～"
+	SummaryTemplate = "新邮件来啦！ #email 标题：[%s]"
+	MessageTemplate = "发件人：`%s`\n\n您可以[点击这里](%s)预览完整内容，或者下载附件查看～"
 )
